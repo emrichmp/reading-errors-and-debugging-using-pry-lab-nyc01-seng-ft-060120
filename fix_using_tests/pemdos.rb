34baa9e002967d6b8a9e9bbae2e7d62a1d859fa2
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-05-23 03:41:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,14 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
+  #binding.pry
   if string[0] == "s"
-    10 * "s" + string
+    binding.pry
+  10 * "s" + string
+  binding.pry
   else
-    string
+  string
   end
 end
+
